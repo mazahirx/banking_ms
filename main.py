0888c0773437f463bf9232535982fec8af4521d0
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-06-06 21:29:20 +0500

    constructer setup & create module added

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import random
+import string
+class Account:
+    accounts = {}
 
+    #constructor
+    def __init__(self, name, acc_id, balance):
+        self.name = name
+        self.acc_id = acc_id
+        self.balance = balance
+
+    #module to create account
+    def create_account(self, accounts):
+        name = input("Enter name : ")
+        characters = string.ascii_letters
+        #generate random acc_id of 5 letters
+        acc_id = "".join(random.choice(characters) for _ in range(5)) 
+
+        for name in accounts:
+            accounts[acc_id] = name
+        
+        print(f"Account created successfully! \nUsername : {name} \nAccount ID: {acc_id}")
