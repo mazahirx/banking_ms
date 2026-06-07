e397ac46357d1e82fe5c9193bc5450c9d8193041
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-06-07 15:35:03 +0500

    removed the para constructor and balance module

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import random
 import string
-class Account:
+class Bank:
     accounts = {}
 
     #constructor
-    def __init__(self, name, acc_id, balance):
-        self.name = name
-        self.acc_id = acc_id
-        self.balance = balance
+    def __init__(self):
+        pass
 
     #module to create account
     def create_account(self):
@@ -39,18 +37,10 @@ class Account:
         else:
             print("Account not found !!")
     
-    #module to add balance
-    def add_balance(self):
-        self.acc_id = input("Enter Account Number : ")
-        self.balance = input("Enter the amount : ")
-
-        for self.acc_id in self.accounts:
-            self.accounts[self.acc_id] = [self.balance]
-            print("Balance added successfully")
     
 def main():
 
-    a1 = Account("Test",25,0)
+    b = Bank()
 
     print("---- BANKING ACCOUNTS ----")
     
@@ -58,19 +48,17 @@ def main():
 
     while True:
         print("\n---- Welcome to the menu ----")
-        print("1.Create Account \n2.Search Account \n3.Delete Account \n4.Add Balance \n5.Exit")
+        print("1.Create Account \n2.Search Account \n3.Delete Account \n4.Exit")
 
         choice = int(input("Chose Option : "))
         
         if choice == 1:
-            a1.create_account()
+            b.create_account()
         elif choice == 2:
-            a1.search_account()
+            b.search_account()
         elif choice == 3:
-            a1.delete_account()
+            b.delete_account()
         elif choice == 4:
-            a1.add_balance()
-        elif choice == 5:
             print("Exiting....")
             break
         else:
