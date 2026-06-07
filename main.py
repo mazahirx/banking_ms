a7028b81cd8b1c094070a912cb1e28a2067917ee
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-06-07 11:37:28 +0500

    Search module added

--- main.py
+++ main.py
@@ -18,5 +18,17 @@ class Account:
 
         for name in accounts:
             accounts[acc_id] = name
-        
-        print(f"Account created successfully! \nUsername : {name} \nAccount ID: {acc_id}")
+            print(f"Account created successfully! \nUsername : {name} \nAccount Number: {acc_id}")
+
+    #module to search account
+    def search_account(self, acc_id):
+        acc_id = input("Enter Account Number : ")
+
+        for acc_id in accounts:
+            if acc_id == accounts:
+                print(f"Account found ! \nUsername : {self.accounts[acc_id]} \nAccount Number : {acc_id}")
+            else:
+                print("Account not found !!")
+    
+
+            
